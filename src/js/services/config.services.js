8d24b3f526cde6d6b799267b48aaff222a1ca5e4
litoxperaloca <pablo.pignolo@gmail.com>
2017-09-08 18:49:23 -0300

    Se quitan los proxies de ionic porque no andan en dispositivos

--- src/js/services/config.services.js
+++ src/js/services/config.services.js
@@ -1,7 +1,7 @@
 pmb_im.services.factory('ConfigService', ['$http', function($http) {
 
   var ConfigObj = {};
-  ConfigObj.baseURL = "/data";
+  ConfigObj.baseURL = "http://dondereciclo.datauy.org";
 
   return ConfigObj;
 
